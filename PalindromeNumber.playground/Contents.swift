3c8188cbeb6847cb0832399448ee61839066902b
dotDustin <logins.dmahone@gmail.com>
2020-05-11 22:57:49 -0400

    progress

---
+++ PalindromeNumber.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+
+    func isPalindrome(_ x: Int) -> Bool {
+        let number = String(x)
+        var palindromeArray = Array(number)
+            
+        while palindromeArray.count > 1 {
+            if palindromeArray.first == palindromeArray.last {
+                palindromeArray.removeFirst()
+                palindromeArray.removeLast()
+            } else {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    let test = 31213
+    
+   isPalindrome(test)
+    
+    
+
--- README.md
+++ README.md
@@ -2,6 +2,18 @@
 
 ## Current Contents
 
+### 9. [Palindrome Number](https://leetcode.com/problems/palindrome-number/)
+
+Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
+
+**Example 1:**
+
+```
+Input: 121
+Output: true
+```
+
+
 ### 273. [Interger to English Words](https://leetcode.com/problems/integer-to-english-words/)
 
 ​	Convert a non-negative integer to its english words representation. Given input is guaranteed to be less than 2^31 - 1.
