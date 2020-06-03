652fa6bfeca2e26c66365c61d4cca6b76ff20f44
dotDustin <logins.dmahone@gmail.com>
2020-06-03 00:01:48 -0400

    rotate string

--- README.md
+++ README.md
@@ -146,3 +146,14 @@ Input: J = "aA", S = "aAAbbbb"
 Output: 3
 ```
 
+### [Rotate String](https://leetcode.com/problems/rotate-string/)
+
+We are given two strings, `A` and `B`.
+
+A *shift on `A`* consists of taking string `A` and moving the leftmost character to the rightmost position. For example, if `A = 'abcde'`, then it will be `'bcdea'` after one shift on `A`. Return `True` if and only if `A` can become `B` after some number of shifts on `A`.
+
+```
+Example 1:
+Input: A = 'abcde', B = 'cdeab'
+Output: true
+```
--- RotateString.playground/Contents.swift
+++ RotateString.playground/Contents.swift
@@ -4,25 +4,30 @@ func rotateString(_ A: String, _ B: String) -> Bool {
     let count = A.count
     var newA = A
     
-    let firstCharacter = newA[newA.startIndex]
-    print(firstCharacter)
-    newA = String(newA.dropFirst())
-    newA = newA.appending(String(firstCharacter))
-    print(newA)
+    if A.count != B.count {
+        return false
+    }
     
-    /*
+    if A == "" {
+        return true
+    }
+    
     for i in 1...count {
         if newA == B {
             return true
         } else {
-            let firstCharacter = newA.startIndex
-            newA = String(newA.removeFirst())
-            print(newA)
+            let firstCharacter = newA[newA.startIndex]
+            
+            newA = String(newA.dropFirst())
+            newA = newA.appending(String(firstCharacter))
         }
-    }*/
+    }
     
     return false
 }
 
+rotateString("a", "")
+rotateString("", "")
 rotateString("abcde", "cdeab")
+rotateString("abcde", "abced")
 
