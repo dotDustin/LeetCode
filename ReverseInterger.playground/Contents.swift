684d65d73ec0bb3b2d4c89a95c4f04f0cc8c5ae9
dotDustin <logins.dmahone@gmail.com>
2020-12-20 23:47:08 -0500

    Reverse Interger setup

---
+++ ReverseInterger.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+class Solution {
+    func reverse(_ x: Int) -> Int {
+        
+        return 1
+    }
+}
--- RomanToInterger.playground/Contents.swift
+++ RomanToInterger.playground/Contents.swift
@@ -1,4 +1,40 @@
+import Foundation
+
+let romanDict = ["I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000]
+let s = "MCMXVII"
+let numbersAsSingleStrings = s.components(separatedBy: "")
+print(numbersAsSingleStrings)
+var numbersInArray: [String] = []
+
 func romanToInt(_ s: String) -> Int {
+//    var numbersAsChars = Array(s)
+    let numbersAsSingleStrings = s.components(separatedBy: "")
+    print(numbersAsSingleStrings)
+    var numbersInArray: [String] = []
+    
+    /*
+    for i in numbersAsSingleStrings {
+        numbersInArray.append(i)
+        print(numbersInArray)
+    }*/
+    
+    /*for (key, value) in numbersAsSingleStrings {
+        numbersInArray.append(value)
+        print(numbersInArray)
+    }*/
+    
+    /*while numbersAsSingleStrings.count > 0 {
         
+    }*/
+    
+    /*for i in numbersAsSingleStrings {
+        
+    }*/
+    
+    
+    
+    return 1
 }
+
+//romanToInt("MCMXVII")
 
